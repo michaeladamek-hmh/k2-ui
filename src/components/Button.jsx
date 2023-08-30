735e176346c652d9cb409d053b10efbdaaf36f9e
Michael Adamek <michael.adamek@hmhco.com>
2023-08-30 15:54:49 -0400

    Updated spaces to tabs.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,27 +2,27 @@ import PropTypes from 'prop-types';
 import CheckIcon from '../assets/scss/images/check.svg';
 
 Button.propTypes = {
-    label: PropTypes.string,
-    primary: PropTypes.bool,
-    correct: PropTypes.bool,
-    iconOnly:  PropTypes.bool,
-    icon: PropTypes.bool
+	label: PropTypes.string,
+	primary: PropTypes.bool,
+	correct: PropTypes.bool,
+	iconOnly:  PropTypes.bool,
+	icon: PropTypes.bool
 }
 
 export function Button({ label, primary, correct, icon, iconOnly }) {
-    const isPrimary = primary ? 'primary' : '';
-    const hasIcon = icon ? 'icon-text' : '';
-    const hasIconOnly = iconOnly ? 'icon-only' : '';
-    const isCorrect = correct ? 'correct' : ''
+	const isPrimary = primary ? 'primary' : '';
+	const hasIcon = icon ? 'icon-text' : '';
+	const hasIconOnly = iconOnly ? 'icon-only' : '';
+	const isCorrect = correct ? 'correct' : ''
 
-    return (
-        <button type="button" className={` ${isPrimary} ${hasIcon} ${isCorrect} ${hasIconOnly} `} >
-            { icon 
-                ? <img src={CheckIcon} />: ''
-            }
-            { iconOnly 
-                ? '' : label 
-            }
-        </button>
-    );
+	return (
+		<button type="button" className={` ${isPrimary} ${hasIcon} ${isCorrect} ${hasIconOnly} `} >
+			{ icon 
+				? <img src={CheckIcon} />: ''
+			}
+			{ iconOnly 
+				? '' : label 
+			}
+		</button>
+	);
 }
